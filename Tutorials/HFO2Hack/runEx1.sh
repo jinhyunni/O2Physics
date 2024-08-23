e9df3a435e565781fc09e22292eb5957e5011631
jinhyunni <pjhhhk@gmail.com>
2024-08-23 17:37:05 +0900

    helper task added

---
+++ Tutorials/HFO2Hack/runEx1.sh
@@ -0,0 +1,6 @@
+OPTION="-b configuration json://myConfig.json"
+
+o2-hfo2hacktut-example1 ${OPTION} |
+o2-analysis-track-propagation ${OPTION} |
+o2-analysis-timestamp ${OPTION} |
+o2-analysis-tracks-extra-converter ${OPTION}
