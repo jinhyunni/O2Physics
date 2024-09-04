eb19ce0095c4823919f1e0db30618d48b180d53b
jinhyunni <pjhhhk@gmail.com>
2024-09-04 22:05:22 +0900

    Example 4 halfway done

--- Tutorials/HFO2Hack/Example4.cxx
+++ Tutorials/HFO2Hack/Example4.cxx
@@ -1,3 +1,14 @@
+// Example4. Calculate avgpT per event
+// 1. Add process switch
+//  
+//    -- Add process switch to do the same task with MC particles
+//
+// 2. Use filter to only track particles within |eta|<0.8
+
+
+
+
+
 #include "Framework/AnalysisTask.h"
 #include "Framework/runDataProcessing.h"
 
@@ -5,6 +16,44 @@ using namespace o2;
 using namespace o2::framework;
 
 struct Example4{
+
+	//Define Histogram registry
+	HistogramRegistry registry{"histo", {}};
+
+	// Add filters
+	Configurable<float> etaCut{"etaCut", 0.8, "eta cut to track"};
+	expressions::Filter etaFilter = aod::track::eta >= -etaCut and aod::track::eta <= etaCut;
+	expressions::Filter etaMCFilter = aod::mcparticle::eta >= -etaCut and aod::mcparticle::eta <= etaCut;
+
+	void init(InitContext const&)
+	{
+		registry.add("hpt", "hpt", {HistType::kTH1F, {{101, -0.05, 10.05}}});
+		registry.add("hptMC", "hptMC", {HistType::kTH1F, {{101, -0.05, 10.05}}});
+		registry.add("havgpt", "havgpt", {HistType::kTH1F, {{21, -0.05, 2.05}}});
+		registry.add("havgptMC", "havgptMC", {HistType::kTH1F, {{101, -0.05, 2.05}}});
+	}
+
+	// Subscribe to filtered table!
+	void process(aod::Collision const& collision, soa::Filtered<aod::Tracks> const& tracks)
+	{
+		float avgpt = 0.;
+
+		if( tracks.size() != 0)
+		{
+
+			for( auto const& track : tracks )
+			{
+				avgpt += track.pt();
+				registry.fill( HIST("hpt"), track.pt());
+			}
+
+			// Divide total summation of pT with total track # in filtered table
+			avgpt /= tracks.size();	
+			registry.fill(HIST("havgpt"), avgpt);
+
+		}// if event has a track within |eta| <0.8
+
+	}
 
 };
 
---
+++ Tutorials/HFO2Hack/runEx4.sh
@@ -0,0 +1,6 @@
+OPTION="-b --aod-file /Users/jinhyunpark/npl/Analysis/ALICE/data/ao2d/O2Tutorial3.0/day1_Introduction/AO2D.root"
+
+o2-hfo2hacktut-example4 ${OPTION} | \
+o2-analysis-track-propagation ${OPTION} | \
+o2-analysis-timestamp ${OPTION} | \
+o2-analysis-tracks-extra-converter ${OPTION}
