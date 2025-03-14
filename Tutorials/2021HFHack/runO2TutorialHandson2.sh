27d91ac15ae2d6f3767ffbc1c005d3c5db27f27b
jinhyunni <pjhhhk@gmail.com>
2025-03-14 18:27:49 +0900

    Added O2Tutorial4.0 handson 2

--- Tutorials/2021HFHack/runO2TutorialHandson2.sh
+++ Tutorials/2021HFHack/runO2TutorialHandson2.sh
@@ -1,9 +1,8 @@
 #OPTION="-b --aod-file /Users/jinhyunpark/npl/Analysis/ALICE/data/ao2d/O2Tutorial4.0/AO2D.root"
-OPTION="-b --configuration json://dpl-config-ex4.json"
+OPTION="-b --configuration json://dpl-config-o2tut-handson2.json"
 
-o2-hfo2hacktut-example4 ${OPTION} | \
+o2-tutorial2024-tutorial2 ${OPTION} | \
 	o2-analysis-track-propagation ${OPTION} | \
 	o2-analysis-timestamp ${OPTION} | \
 	o2-analysis-event-selection ${OPTION} | \
-	o2-analysis-tracks-extra-v002-converter ${OPTION} | \
-	o2-analysis-multiplicity-table ${OPTION} 
+	o2-analysis-tracks-extra-v002-converter ${OPTION}
